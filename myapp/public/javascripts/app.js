77b6e15aef47fd4992959beb0f272ceea563fb03
Teisted <mathan3322@gmail.com>
2020-01-13 22:09:07 +0100

    Messy code but added game and player code kinda

--- myapp/public/javascripts/app.js
+++ myapp/public/javascripts/app.js
@@ -1,6 +1,8 @@
 var main = function() {
-	const board = new Board('#board')
-	const player = new Player("rgb(64, 224, 0)");
+	const board = new Board('#board');
+	const playerB = new Player("rgb(48, 207, 160)", "Player 2");
+	const playerA = new Player("rgb(218, 187, 53)", "Player 1");
+	const game = new Game(0, board, playerA, playerB, '#timer');
 }
 
 $(document).ready(main)
--- myapp/public/javascripts/board.js
+++ myapp/public/javascripts/board.js
@@ -4,7 +4,7 @@ class Board {
     this.col = 7;
     this.board = board;
     this.createBoard();
-    this.listenMouseEvent();
+    //this.listenMouseEvent();
   }
 
   createBoard() {
@@ -24,54 +24,20 @@ class Board {
       $board.append($row);
     }
   }
-
-  listenMouseEvent() {
-    function dropAnimation(colValue, pColor) {
-      let col = $("[col=" + colValue + "]");
-      for (let i = 1; i < col.length; i++) {
-        let $colNext = $(col[i + 1]);
-        let $col = $(col[i]);
-        
-        //Have to change this vvv
-        
-        if ($col.css("border-top-color") == pColor) break; 
-        let color = $colNext.css("border-top-color");
-        //check white color
-        if (color == "rgb(255, 255, 255)") {
-          setTimeout(function() {
-            $col.css("border-color", pColor);
-          }, i * 20);
-          setTimeout(function() {
-            $col.css("border-color", "white");
-          }, i * 20 + 20);
-        } else {
-          setTimeout(function() {
-            $col.css("border-color", pColor);
-          }, i * 20);
-          return true;
-        }
-      }
-      return false;
-    }
-
-    $("[col]").click(function(event, color) {
-      let colValue = event.target.getAttribute("col");
-      let rowValue = event.target.getAttribute("row");
-      let emptyRing = dropAnimation(colValue, color);
-      console.log(emptyRing);
-    });
-  }
+   
 }
 
 
 class Player {
-  constructor(color) {
+  constructor(color, name) {
+    this.name = name;
     this.color = color;
     this.ingameListener(this);
     
   }
 
   ingameListener(player) {
+    console.log(this);
     $("[col]").mouseenter(function(event) {
       let colValue = event.target.getAttribute("col");
       $("[row = 0][col=" + colValue + "]").css("border-color", player.color);
@@ -85,11 +51,80 @@ class Player {
 }
 
 class Game {
-  constructor(gameId, board, playerA, playerB){
+  constructor(gameId, board, playerA, playerB, timer){
     this.playerA = playerA;
     this.playerB = playerB;
     this.board = board;
     this.gameId = gameId;
+    this.currentTurn = playerA;
+    this.timer = timer;
+
+    this.runTimer(this, 3);
+    this.clickFunc(this, this.board, this.currentTurn);
+  }
+
+  runTimer(game, i){
+    $(game.timer).html(""+i);
+
+    setInterval(function(){
+      i--;
+      if(i < 0){
+        i = 3;
+        if(game.currentTurn == game.playerA){
+          game.currentTurn = game.playerB;
+          game.setCurrentPlayer(game.currentTurn);
+        }
+        else {
+          game.currentTurn = game.playerA;
+          game.setCurrentPlayer(game.currentTurn);
+        }
+      }
+      $(timer).html(""+i);
+    }, 1000)
+  }
+
+  clickFunc(game, board) {
+    
+    $("[col]").click(function(event) {
+      let colValue = event.target.getAttribute("col");
+      let rowValue = event.target.getAttribute("row");
+      let color = game.currentTurn.color;
+      let emptyRing = game.dropAnimation(colValue, color);
+      console.log(emptyRing);
+    });
+  }
+
+  setCurrentPlayer(player){
+    player.ingameListener(player);
+    $('#playerTurn').html(player.name).css("color", player.color);
+  }
+
+  dropAnimation(colValue, pColor) { //seems like a generic function.
+    let col = $("[col=" + colValue + "]");
+    for (let i = 1; i < col.length; i++) {
+      let $colNext = $(col[i + 1]);
+      let $col = $(col[i]);
+      
+      //Have to change this vvv
+      
+      if ($col.css("border-top-color") == pColor) break; 
+      let color = $colNext.css("border-top-color");
+      //check white color
+      if (color == "rgb(255, 255, 255)") {
+        setTimeout(function() {
+          $col.css("border-color", pColor);
+        }, i * 20);
+        setTimeout(function() {
+          $col.css("border-color", "white");
+        }, i * 20 + 20);
+      } else {
+        setTimeout(function() {
+          $col.css("border-color", pColor);
+        }, i * 20);
+        return true;
+      }
+    }
+    return false;
   }
 
 }
