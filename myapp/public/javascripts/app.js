675f4cc61a129abb4d5ae4e659e4c6ed03c068a3
Teisted <mathan3322@gmail.com>
2020-01-15 16:10:07 +0100

    updating client/server

--- myapp/public/javascripts/app.js
+++ myapp/public/javascripts/app.js
@@ -1,8 +1,29 @@
 var main = function() {
   const board = new Board("#board");
-  const playerB = new Player("rgb(48, 207, 160)", "Player 2");
-  const playerA = new Player("rgb(218, 187, 53)", "Player 1");
-  const game = new Game(0, board, playerA, playerB, 10);
+  var player;
+
+  
+  //THIS SHIT IS THE CLIENT VVVVVVVVVVVVV DONT FORGET
+  var socket = new WebSocket("ws://localhost:3000");
+  socket.onopen = function(){
+    socket.send("Socket Opened");
+  };
+
+  socket.onmessage = function(message){
+    var data = JSON.parse(message.data)
+    console.log(data);
+    if(data.type == 'PLAYER_ID'){
+      if(data.id == 1){
+        player = new Player("rgb(218, 187, 53)", "Player 1", 1);
+        console.log("HELP MMEMEMEME")
+      }
+      else if(data.id == 2) {
+        player = new Player("rgb(48, 207, 160)", "Player 2", 2);
+      }
+    }
+  }
+
+  
 };
 
 $(document).ready(main);
